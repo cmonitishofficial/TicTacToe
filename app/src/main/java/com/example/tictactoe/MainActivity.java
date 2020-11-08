2419b7538b0bf67e2eb7dd031999f87681efaf47
Nitish Kumar <niralanitrogensir@gmail.com>
2020-11-08 09:50:20 +0530

    In this Commit , I Have learned all about Game Logic and also I design this layout.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+//    0-x;
+//    1-O;
+//    2-Null;
+    int activePlayer = 0;
+    int []gameState={2,2,2,2,2,2,2,2,2}
+
+    public void playTap(View view) {
     }
 }
